46f483fbd953c063a1296d1bf628257d1b056e87
JaeWong91 <Jaewong19@hotmail.co.uk>
2021-04-10 17:12:39 +0000

    add missing semi colon

--- static/js/script.js
+++ static/js/script.js
@@ -18,4 +18,4 @@ output.innerHTML = slider.value; // Display the default slider value
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = this.value;
-}
+};
